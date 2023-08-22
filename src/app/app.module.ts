34a41dcab3eb297ef8dd0f069673b461e22efe77
lmenon <lavanyamenon1215@gmail.com>
2023-08-22 15:54:14 -0600

    got extension and local angular serve working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './welcome-page/welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
